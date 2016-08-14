5d3ed009c48e2f2e2c1a5cfcbb17a945e2c827f7
Samuel Lam <samuelkinlam@gmail.com>
2016-08-14 11:20:27 -0400

    comment code

--- app.js
+++ app.js
@@ -10,35 +10,30 @@ const cheerio = require('cheerio');
 const app = express();
 app.use(bodyParser.json());
 
-// request('https://news.ycombinator.com/newest', (err, res, html) => {
-//   let $ = cheerio.load(html);
-//   console.log($('title').text());
-//   // => News Link | Hacker News
-// });
-
 const hackerNewsData = {};
 
+/* Note that the below may not be a robust solution as HN's site and the article format may change from time to time - factoring in corner cases. Adapt as needed. */
 request('https://news.ycombinator.com/newest', (err, res, html) => {
   const $ = cheerio.load(html);
 
   for (let i = 1; i <= 88; i+=3) {
     // grabs the needed table Row
-    var tableRow = `table tr:nth-child(3) table tr:nth-child(${i})`;
+    let tableRow = `table tr:nth-child(3) table tr:nth-child(${i})`;
 
     // grab article rank
     let rank = $(`${tableRow} span`).html().slice(0, -1);
     hackerNewsData[rank] = {};
 
-    // grab urlLink and title which are in same <td>
+    // grab urlLink, title and site which are in same <td>
     let extractTd = `${tableRow} td:nth-child(3)`;
 
+    // grab urlLink
     let urlLink = $(`${extractTd} a`).attr().href;
 
+    // steps to grab and format title and site
     let header = $(`${extractTd}`).first().text();
     let lastIdx = header.lastIndexOf(' ');
     let title = header.substring(0, lastIdx);
-
-
     let site = header.slice(lastIdx+2, -1);
 
     hackerNewsData[rank]['url'] = urlLink;
@@ -46,7 +41,8 @@ request('https://news.ycombinator.com/newest', (err, res, html) => {
     hackerNewsData[rank]['site'] = site;
   }
   fs.writeFile('hackerNews.json', JSON.stringify(hackerNewsData, null, 4), err => {
-    console.log('Created successfully!');
+    const msg = err || 'Created successfully!';
+    console.log(msg);
   });
 });
 
